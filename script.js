5ac427db42451ff65aff151cfe3dfec9af0d8dbd
Axe11er <Axeller@mail.ru>
2021-06-05 22:26:16 +0800

    toggle menu links

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let menuLinks = document.querySelectorAll('.sidebar__menu-item-link');
 let menuLink = document.querySelector('.sidebar__menu-item-link');
 
-for (i = 0; i < menuLinks.length; i++) {
-	menuLinks[i].addEventListener('mouseover', function() {
+for (let i = 0; i < menuLinks.length; i++) {
+	
+	menuLinks[i].addEventListener('click', function () {
+		for (let j = 0; j < menuLinks.length; j++) {
+			menuLinks[j].classList.remove('sidebar__menu-item-link--active');
+		}
 		menuLinks[i].classList.add('sidebar__menu-item-link--active');
 	});
 }
