73c54f9484776262a6abd37a2f58fad8361b6928
Axe11er <Axeller@mail.ru>
2021-06-16 17:56:53 +0800

    1.5

--- cps 1.4/script.js
+++ cps 1.4/script.js
@@ -1,8 +1,21 @@
 let menuLinks = document.querySelectorAll('.sidebar__menu-item-link');
-let menuLink = document.querySelector('.sidebar__menu-item-link');
 
-for (i = 0; i < menuLinks.length; i++) {
-	menuLinks[i].addEventListener('mouseover', function() {
+for (let i = 0; i < menuLinks.length; i++) {
+	menuLinks[i].addEventListener('click', function () {
+		for (let j = 0; j < menuLinks.length; j++) {
+			menuLinks[j].classList.remove('sidebar__menu-item-link--active');
+		}
 		menuLinks[i].classList.add('sidebar__menu-item-link--active');
+	});
+}
+
+let languageLinks = document.querySelectorAll('.sidebar__language-list-item-link');
+
+for (let i = 0; i < languageLinks.length; i++) {
+	languageLinks[i].addEventListener('click', function () {
+		for (let j = 0; j < languageLinks.length; j++) {
+			languageLinks[j].classList.remove('sidebar__language-list-item-link--active');
+		}
+		languageLinks[i].classList.add('sidebar__language-list-item-link--active');
 	});
 }
